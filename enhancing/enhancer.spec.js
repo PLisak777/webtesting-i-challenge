3dbd1c9d2d0af007abcf2e706bd68e77da0336cf
Phil Lisak <phil.lisak@gmail.com>
2020-12-08 18:30:09 -0700

    Tests for repair() implemented and passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,18 +2,39 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 describe('enhancer.js', () => {
+	describe('repair()', () => {
+		test('repair() returns new object', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 15,
+				durability: 50,
+			};
+			const result = enhancer.repair(item);
 
-    describe('repair()', () => {
+			expect(result).not.toBe(item);
+		});
 
-        test('repair() returns new object', ()=> {
-            const item = { 
-                name: 'foo',
-                enhancement: 15,
-                durability: 50
-            }
-            const result = enhancer.repair(item)
+		test('repair() returns new item with 100 dur', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 15,
+				durability: 50,
+			};
+			const result = enhancer.repair(item);
 
-            expect(result).not.toBe(item)
-        })
-    })
-})
+			expect(result.durability).toBe(100);
+		});
+
+		test('repair() will only change durability stat', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 15,
+				durability: 50,
+			};
+			const result = enhancer.repair(item);
+
+			expect(result.name).toBe('foo');
+			expect(result.enhancement).toBe(15);
+		});
+	});
+});
