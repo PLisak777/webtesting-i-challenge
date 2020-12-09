0b041edae9569a70831081598902473c63312ae3
Phil Lisak <phil.lisak@gmail.com>
2020-12-08 19:03:03 -0700

    MVP and Stretch complete. Get() method implemented and tested.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -95,39 +95,75 @@ describe('enhancer.js', () => {
 			const result = enhancer.fail(item);
 
 			expect(result).not.toBe(item);
-        });
-        
-        test('fail() decreases DUR by 5 when ENH < 15', () => {
-            const item = {
-                name: 'foo',
-                enhancement: 14, 
-                durability: 50
-            }
-            const result = enhancer.fail(item)
+		});
 
-            expect(result.durability).toBe(45)
-        })
+		test('fail() decreases DUR by 5 when ENH < 15', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 14,
+				durability: 50,
+			};
+			const result = enhancer.fail(item);
 
-        test('fail() decreases DUR by 10 when ENH >= 15', () => {
-            const item = {
-                name: 'foo',
-                enhancement: 15, 
-                durability: 50
-            }
-            const result = enhancer.fail(item)
+			expect(result.durability).toBe(45);
+		});
 
-            expect(result.durability).toBe(40);
-        })
+		test('fail() decreases DUR by 10 when ENH >= 15', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 15,
+				durability: 50,
+			};
+			const result = enhancer.fail(item);
 
-        test('fail() decreases ENH -1 when ENH > 16', () => {
-            const item = {
-                name: 'foo',
-                enhancement: 17, 
-                durability: 50
-            }
-            const result = enhancer.fail(item)
+			expect(result.durability).toBe(40);
+		});
 
-            expect(result.enhancement).toBe(16);
-        })
+		test('fail() decreases ENH -1 when ENH > 16', () => {
+			const item = {
+				name: 'foo',
+				enhancement: 17,
+				durability: 50,
+			};
+			const result = enhancer.fail(item);
+
+			expect(result.enhancement).toBe(16);
+		});
+	});
+
+	describe('get()', () => {
+		test('get() returns new object', () => {
+			const item = {
+				name: 'Sword of Badasstitude',
+				enhancement: 7,
+				durability: 50,
+			};
+			const result = enhancer.get(item);
+
+			expect(result).not.toBe(item);
+		});
+
+		test('get() correctly modifies obj name', () => {
+			const item = {
+				name: 'Sword of Badasstitude',
+				enhancement: 7,
+				durability: 50,
+			};
+			const result = enhancer.get(item);
+
+			expect(result.name).toBe('[+7] Sword of Badasstitude');
+		});
+
+		test('get() only modifies name property', () => {
+			const item = {
+				name: 'Sword of Badasstitude',
+				enhancement: 7,
+				durability: 50,
+			};
+			const result = enhancer.get(item);
+
+			expect(result.enhancement).toBe(7);
+			expect(result.durability).toBe(50);
+		});
 	});
 });
